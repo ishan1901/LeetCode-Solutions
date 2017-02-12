fbffbf8fd0e875f9fe3f0766f77f305a4299a605
Ishan Sharma <ishan.sharma091@gmail.com>
2017-02-12 11:53:53 -0500

    Update Rotate.java

--- Rotate.java
+++ Rotate.java
@@ -29,4 +29,28 @@ public class Rotate {
         nums[a] = nums[b];
         nums[b] = temp;
     }
+    
+    // solution 2
+    public void rotate2(int[] nums, int k) {
+        if (k > nums.length) { k = k % nums.length; }
+        if(nums.length <= 1 || nums.length == k || k == 0) {
+            return;
+        }
+        reverse(nums, 0, nums.length - 1);
+        reverse(nums, 0, k - 1);
+        reverse(nums, k, nums.length - 1);
+        
+
+    }
+    
+    public void reverse(int[] nums, int a, int b) {
+        int length = b - a;
+        for (int i = 0; i <= length/2; i++) {
+            int temp = nums[a];
+            nums[a] = nums[b];
+            nums[b] = temp;
+            b--;
+            a++;
+        }
+    }
 }
