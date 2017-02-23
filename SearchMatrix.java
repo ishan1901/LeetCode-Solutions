58b18f7045ec4f9e97919af3912c1d557fdacd1a
Ishan Sharma <ishan.sharma091@gmail.com>
2017-02-23 00:04:26 -0500

    Create SearchMatrix.java

---
+++ SearchMatrix.java
@@ -0,0 +1,17 @@
+public class SearchMatrix {
+    public boolean searchMatrix(int[][] matrix, int target) {
+        if (matrix == null || matrix.length < 1) {return false;}
+        int row = 0;
+        int col = matrix[0].length - 1;
+        while(row < matrix.length && col >= 0) {
+            if (matrix[row][col] == target) {
+                return true;
+            } else if (target < matrix[row][col]) {
+                col--;
+            } else if (target > matrix[row][col]) {
+                row++;
+            }
+        }
+        return false;
+    }
+}
